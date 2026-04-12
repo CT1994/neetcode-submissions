45f9348b91d1e2139e9f7b97db579dd44bf0ea43
CT1994 <CT1994@users.noreply.github.com>
2026-04-12 21:23:01 +0100

    Add: dynamicArray - submission-0

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.js
@@ -0,0 +1,73 @@
+class DynamicArray {
+    /**
+     * @constructor
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.length = 0;
+        this.array = new Array(capacity).fill(0)
+    }
+
+    /**
+     * @param {number} i
+     * @returns {number}
+     */
+    get(i) {
+        return this.array[i]
+    }
+
+    /**
+     * @param {number} i
+     * @param {number} n
+     * @returns {void}
+     */
+    set(i, n) {
+        this.array[i] = n;
+        if (this.length < i) {
+            this.length += 1;
+        }
+    }
+
+    /**
+     * @param {number} n
+     * @returns {void}
+     */
+    pushback(n) {
+        if (this.length === this.capacity) {
+            this.resize();
+        }
+        this.array[this.length] = n;
+        this.length += 1;
+    }
+
+    /**
+     * @returns {number}
+     */
+    popback() {
+        const val = this.array[this.length - 1];
+        this.length -= 1;
+        return val;
+    }
+
+    /**
+     * @returns {void}
+     */
+    resize() {
+        this.capacity *= 2
+    }
+
+    /**
+     * @returns {number}
+     */
+    getSize() {
+        return this.length;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getCapacity() {
+        return this.capacity;
+    }
+}
