4d15461d5cbe095f4f7a619309504bf818eb28d0
CT1994 <CT1994@users.noreply.github.com>
2026-06-28 23:37:38 +0100

    Add: design-linked-list - submission-1

---
+++ Data Structures & Algorithms/design-linked-list/submission-1.js
@@ -0,0 +1,107 @@
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class MyLinkedList {
+    constructor() {
+        this.head = new ListNode(-1);
+        this.tail = new ListNode(-1);
+
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+    /**
+     * @param {number} index
+     * @return {number}
+     */
+    get(index) {
+        let cur = this.head.next;
+
+        while (index && cur) {
+            index--;
+            cur = cur.next;
+        }
+
+        if (index === 0 && cur) {
+            return cur.val;
+        }
+
+        return -1;
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    addAtHead(val) {
+        const node = new ListNode(val);
+        const prev = this.head;
+        const next = this.head.next;
+        node.prev = prev;
+        node.next = next;
+        prev.next = node;
+        next.prev = node;
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    addAtTail(val) {
+        const node = new ListNode(val);
+        const prev = this.tail.prev;
+        const next = this.tail;
+        node.prev = prev;
+        node.next = next;
+        prev.next = node;
+        next.prev = node;
+    }
+
+    /**
+     * @param {number} index
+     * @param {number} val
+     * @return {void}
+     */
+    addAtIndex(index, val) {
+        let cur = this.head.next;
+        while (cur && index) {
+            cur = cur.next;
+            index--;
+        }
+
+        if (cur && index === 0) {
+            const node = new ListNode(val);
+            const prev = cur.prev;
+            const next = cur;
+            node.prev = prev;
+            node.next = next;
+            prev.next = node;
+            next.prev = node;
+        }
+    }
+
+    /**
+     * @param {number} index
+     * @return {void}
+     */
+    deleteAtIndex(index) {
+        let cur = this.head.next;
+
+        while (index && cur) {
+            cur = cur.next;
+            index--;
+        }
+
+        if (index === 0 && cur !== this.tail) {
+            const prev = cur.prev;
+            const next = cur.next;
+            prev.next = next;
+            next.prev = prev;
+        }
+    }
+}
