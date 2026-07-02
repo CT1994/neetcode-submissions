d49d50f8404ae16863b493db7c70941a87e944a9
CT1994 <CT1994@users.noreply.github.com>
2026-07-02 21:40:57 +0100

    Add: singlyLinkedList - submission-10

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-10.js
@@ -0,0 +1,88 @@
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = new ListNode(-1);
+        this.tail = this.head;
+    }
+
+    /**
+     * @param {number} index
+     * @return {number}
+     */
+    get(index) {
+        let cur = this.head.next;
+        let i = 0;
+        while (cur) {
+            if (i === index) {
+                return cur.val;
+            }
+            i++;
+            cur = cur.next;
+        }
+        return -1;
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    insertHead(val) {
+        const node = new ListNode(val);
+        node.next = this.head.next;
+        this.head.next = node;
+        if (!node.next) {
+            this.tail = node;
+        }
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    insertTail(val) {
+        const node = new ListNode(val);
+        this.tail.next = node;
+        this.tail = this.tail.next;
+    }
+
+    /**
+     * @param {number} index
+     * @return {boolean}
+     */
+    remove(index) {
+        let cur = this.head;
+        while (index && cur) {
+            cur = cur.next;
+            index--;
+        }
+
+        if (cur && cur.next) {
+            if (cur.next === this.tail) {
+                this.tail = cur;
+            }
+            cur.next = cur.next.next;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return {number[]}
+     */
+    getValues() {
+        const res = [];
+        let cur = this.head.next;
+        while (cur) {
+            res.push(cur.val);
+            cur = cur.next;
+        }
+        return res;
+    }
+}
