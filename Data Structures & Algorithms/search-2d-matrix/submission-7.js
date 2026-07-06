331ec714cc90ee36fe43ebad84bb95705676e74d
CT1994 <CT1994@users.noreply.github.com>
2026-07-06 11:53:05 +0100

    Add: search-2d-matrix - submission-7

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-7.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const rows = matrix.length;
+        const cols = matrix[0].length;
+
+        let l = 0;
+        let r = rows * cols - 1;
+
+        while (l <= r) {
+            const m = l + Math.floor((r - l) / 2);
+            const row = Math.floor(m / cols);
+            const col = m % cols;
+
+            if (matrix[row][col] === target) {
+                return true;
+            }
+            else if (matrix[row][col] < target) {
+                l = m + 1;
+            }
+            else {
+                r = m - 1;
+            }
+        }
+
+        return false;
+    }
+}
