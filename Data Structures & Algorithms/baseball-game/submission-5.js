8c177efc18ac854aa19167dad8dba5eb55196088
CT1994 <CT1994@users.noreply.github.com>
2026-07-09 17:22:12 +0100

    Add: baseball-game - submission-5

---
+++ Data Structures & Algorithms/baseball-game/submission-5.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        const stack = [];
+        let total = 0;
+
+        for (const o of operations) {
+            switch (o) {
+                case "+":
+                    stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+                    total += stack[stack.length - 1];
+                    break;
+                case "D":
+                    stack.push(stack[stack.length - 1] * 2);
+                    total += stack[stack.length - 1];
+                    break;
+                case "C":
+                    total -= stack.pop();
+                    break;
+                default:
+                    stack.push(Number(o));
+                    total += Number(o);
+            }
+        }
+
+        return total;
+    }
+}
