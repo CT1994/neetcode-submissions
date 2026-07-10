14470d20d1638966407253bdd3764197c19980a2
CT1994 <CT1994@users.noreply.github.com>
2026-07-10 15:04:17 +0100

    Add: max-consecutive-ones - submission-3

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-3.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let res = 0;
+        let count = 0;
+
+        for (const n of nums) {
+            count = n === 1 ? count + 1 : 0;
+            res = Math.max(res, count);
+        }
+
+        return res;
+    }
+}
