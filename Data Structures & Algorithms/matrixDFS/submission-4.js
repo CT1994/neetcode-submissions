3a6668c729b50f2f394cb40afd106e71fb6358ed
CT1994 <CT1994@users.noreply.github.com>
2026-07-11 18:47:33 +0100

    Add: matrixDFS - submission-4

---
+++ Data Structures & Algorithms/matrixDFS/submission-4.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @returns {number}
+     */
+    countPaths(grid) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+        let res = 0;
+
+        const dfs = (r, c) => {
+            const key = r * cols + c;
+            if (r < 0 || c < 0 || r === rows || c === cols || grid[r][c] === 1) {
+                return;
+            }
+
+            if (r === rows - 1 && c === cols - 1) {
+                res++;
+                return;
+            }
+
+            grid[r][c] = 1;
+            dfs(r + 1, c);
+            dfs(r, c + 1);
+            dfs(r - 1, c);
+            dfs(r, c - 1);
+            grid[r][c] = 0;
+        };
+
+        dfs(0, 0);
+        return res;
+    }
+}
