1ab9d04cd71d71be2bd3a9dc0505e26182afcc70
CT1994 <CT1994@users.noreply.github.com>
2026-07-15 08:55:36 +0100

    Add: matrixBFS - submission-5

---
+++ Data Structures & Algorithms/matrixBFS/submission-5.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number[][]}
+     * @returns {number}
+     */
+    shortestPath(grid) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+        const directions = [
+            [1, 0],
+            [0, 1],
+            [-1, 0],
+            [0, -1],
+        ];
+        let res = 0;
+        const q = new Queue();
+        q.push([0, 0]);
+        grid[0][0] = 1;
+
+        while (!q.isEmpty()) {
+            const length = q.size();
+            for (let i = 0; i < length; i++) {
+                const [or, oc] = q.pop();
+
+                if (or === rows - 1 && oc === cols - 1) {
+                    return res;
+                }
+
+                for (const [dr, dc] of directions) {
+                    const r = or + dr;
+                    const c = oc + dc;
+                    const key = r * cols + c;
+                    if (r < 0 || c < 0 || r === rows || c === cols || grid[r][c] === 1) {
+                        continue;
+                    }
+
+                    q.push([r, c]);
+                    grid[r][c] = 1;
+                }
+            }
+            res++;
+        }
+
+        return -1;
+    }
+}
