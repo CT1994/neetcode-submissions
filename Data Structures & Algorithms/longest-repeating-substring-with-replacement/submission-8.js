fed76b5d999af1767a3d9b4e16707b86dabff24e
CT1994 <CT1994@users.noreply.github.com>
2026-07-16 15:36:45 +0100

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        const freq = new Uint16Array(26);
+        const offset = "A".charCodeAt(0);
+
+        let res = 0;
+        let L = 0;
+        let maxF = 0;
+        for (let R = 0; R < s.length; R++) {
+            const rChar = s.charCodeAt(R) - offset;
+            freq[rChar]++;
+            maxF = Math.max(maxF, freq[rChar]);
+
+            while (R - L + 1 - maxF > k) {
+                freq[s.charCodeAt(L) - offset]--;
+                L++;
+            }
+
+            res = Math.max(res, R - L + 1)
+        }
+
+        return res;
+    }
+}
