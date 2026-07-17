ff00f61b14102cfea3acd7d93efe30e8d7f97855
CT1994 <CT1994@users.noreply.github.com>
2026-07-18 00:09:48 +0100

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let res = 0;
+        let l = 0;
+        let r = heights.length - 1;
+
+        while (l < r) {
+            const contain = Math.min(heights[l], heights[r]) * (r - l);
+            res = Math.max(res, contain);
+            if (heights[l] < heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return res;
+    }
+}
