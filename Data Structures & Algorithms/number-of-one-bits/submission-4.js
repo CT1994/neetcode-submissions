12be4f142c827df8d2f45f5d38498d15b19a6aa5
CT1994 <CT1994@users.noreply.github.com>
2026-07-17 21:40:21 +0100

    Add: number-of-one-bits - submission-4

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-4.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n) {
+        let count = 0;
+        while (n) {
+            if (n & (1 === 1)) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+}
