ddaad7b5dfc5c3c5e868b6c1bbd11eeecb11deef
CT1994 <CT1994@users.noreply.github.com>
2026-07-22 10:14:28 +0100

    Add: combinations-of-a-phone-number - submission-4

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-4.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} digits
+     * @return {string[]}
+     */
+    letterCombinations(digits) {
+        if (digits.length === 0) return [];
+        const res = [];
+        const digitToChar = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz",
+        };
+        const dfs = (i, curStr) => {
+            if (i === digits.length) {
+                res.push(curStr);
+                return;
+            }
+
+            for (const c of digitToChar[digits[i]]) {
+                dfs(i + 1, curStr + c);
+            }
+        };
+        dfs(0, "");
+        return res;
+    }
+}
