7961331c4787201b705954725108e79031ad3abe
CT1994 <CT1994@users.noreply.github.com>
2026-07-25 12:07:46 +0100

    Add: course-schedule-iv - submission-5

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-5.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @param {number[][]} queries
+     * @return {boolean[]}
+     */
+    checkIfPrerequisite(numCourses, prerequisites, queries) {
+        const adjList = new Array(numCourses);
+        const prereqMap = new Map();
+        for (let i = 0; i < numCourses; i++) {
+            adjList[i] = new Set();
+        }
+
+        for (let [pre, csr] of prerequisites) {
+            adjList[csr].add(pre);
+        }
+
+        const dfs = (i) => {
+            if (prereqMap.has(i)) return prereqMap.get(i);
+            const prereqs = new Set();
+
+            for (const n of adjList[i]) {
+                for (const p of dfs(n)) prereqs.add(p);
+            }
+            prereqs.add(i)
+            prereqMap.set(i, prereqs);
+            return prereqs;
+        };
+
+        for (let i = 0; i < numCourses; i++) {
+            dfs(i);
+        }
+
+        return queries.map(([u, v]) => prereqMap.get(v).has(u));
+    }
+}
