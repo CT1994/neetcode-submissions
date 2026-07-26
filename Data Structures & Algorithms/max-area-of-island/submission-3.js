c0cd8964a3b9d86be8bb38daf6062e09974958fa
CT1994 <CT1994@users.noreply.github.com>
2026-07-26 10:18:53 +0100

    Add: max-area-of-island - submission-3

---
+++ Data Structures & Algorithms/max-area-of-island/submission-3.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    maxAreaOfIsland(grid) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+
+        const dfs = (r, c) => {
+            if (r < 0 || c < 0 || r === rows || c === cols || grid[r][c] === 0) {
+                return 0;
+            }
+
+            grid[r][c] = 0;
+            let count = 1;
+
+            count += dfs(r + 1, c);
+            count += dfs(r, c + 1);
+            count += dfs(r - 1, c);
+            count += dfs(r, c - 1);
+
+            return count;
+        };
+
+        let res = 0;
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                res = Math.max(res, dfs(r, c));
+            }
+        }
+        return res;
+    }
+}
