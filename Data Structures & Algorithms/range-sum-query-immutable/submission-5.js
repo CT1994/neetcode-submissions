4eea85013e7de7b7f109f45877c2637455d8dfda
CT1994 <CT1994@users.noreply.github.com>
2026-07-27 15:05:55 +0100

    Add: range-sum-query-immutable - submission-5

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-5.js
@@ -0,0 +1,20 @@
+class NumArray {
+    /**
+     * @param {number[]} nums
+     */
+    constructor(nums) {
+        this.prefix = new Array(nums.length);
+        for (let i = 0; i < nums.length; i++) {
+            this.prefix[i] = (this.prefix[i - 1] || 0) + nums[i];
+        }
+    }
+
+    /**
+     * @param {number} left
+     * @param {number} right
+     * @return {number}
+     */
+    sumRange(left, right) {
+        return this.prefix[right] - (this.prefix[left - 1] || 0);
+    }
+}
