badb7d6e220e4855b8dc6edd782f412bdf06bbe7
CT1994 <CT1994@users.noreply.github.com>
2026-07-27 14:54:10 +0100

    Add: longest-turbulent-subarray - submission-6

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-6.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number}
+     */
+    maxTurbulenceSize(arr) {
+        let l = 0;
+        let r = 1;
+        let res = 1;
+        let prev = "";
+
+        while (r < arr.length) {
+            if (arr[r] > arr[r - 1] && prev !== ">") {
+                res = Math.max(res, r - l + 1);
+                r++;
+                prev = ">";
+            } else if (arr[r] < arr[r - 1] && prev !== "<") {
+                res = Math.max(res, r - l + 1);
+                r++;
+                prev = "<";
+            } else {
+                r = arr[r] === arr[r - 1] ? r + 1 : r;
+                l = r - 1;
+                prev = "";
+            }
+        }
+
+        return res;
+    }
+}
