d0bcbf2769a2adb3183b30fa062ea6b807139187
CT1994 <CT1994@users.noreply.github.com>
2026-07-27 15:21:12 +0100

    Add: prim - submission-5

---
+++ Data Structures & Algorithms/prim/submission-5.js
@@ -0,0 +1,40 @@
+/**
+ * const PriorityQueue = require('priority-queue-js');
+ */
+
+class Solution {
+    /**
+     * @param {number} n
+     * @param {Array<Array<number>>} edges
+     * @returns {number}
+     */
+    minimumSpanningTree(n, edges) {
+        const adjList = [];
+        for (let i = 0; i < n; i++) {
+            adjList[i] = [];
+        }
+
+        for (const [n1, n2, w] of edges) {
+            adjList[n1].push([w, n2]);
+            adjList[n2].push([w, n1]);
+        }
+
+        const minHeap = new MinPriorityQueue((val) => val[0]);
+        minHeap.push([0, 0]);
+        const visit = new Set();
+        let res = 0;
+        while (!minHeap.isEmpty()) {
+            const [w1, n1] = minHeap.pop();
+            if (visit.has(n1)) continue;
+            visit.add(n1);
+            res += w1;
+            for (const [w2, n2] of adjList[n1]) {
+                if (!visit.has(n2)) {
+                    minHeap.push([w2, n2]);
+                }
+            }
+        }
+
+        return visit.size === n ? res : -1;
+    }
+}
