d246795f94e2737f20bf8869861c6fe8ea109056
CT1994 <CT1994@users.noreply.github.com>
2026-07-28 10:54:00 +0100

    Add: last-stone-weight-ii - submission-6

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-6.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeightII(stones) {
+        const total = stones.reduce((a, b) => a + b, 0);
+        const target = Math.floor(total / 2);
+        const dp = Array.from({ length: stones.length }, () => new Array(target + 1).fill(-1));
+        const dfs = (i, sum) => {
+            if (sum >= target || i === stones.length) {
+                return Math.abs(sum - (total - sum));
+            }
+            if (dp[i][sum] !== -1) return dp[i][sum];
+            return (dp[i][sum] = Math.min(dfs(i + 1, sum), dfs(i + 1, sum + stones[i])));
+        };
+        return dfs(0, 0)
+    }
+}
