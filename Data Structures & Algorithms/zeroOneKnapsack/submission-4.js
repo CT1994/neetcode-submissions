ba8c02032532c6240e04350581ca53a599dab945
CT1994 <CT1994@users.noreply.github.com>
2026-07-28 10:11:47 +0100

    Add: zeroOneKnapsack - submission-4

---
+++ Data Structures & Algorithms/zeroOneKnapsack/submission-4.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {<Array<number>} profit
+     * @param {<Array<number>} weight
+     * @param {number} capacity
+     * @returns {number}
+     */
+    maximumProfit(profit, weight, capacity) {
+        const rows = profit.length;
+        const cols = capacity + 1;
+        let dp = new Array(cols).fill(0);
+
+        for (let c = 0; c < cols; c++) {
+            if (weight[0] <= c) {
+                dp[c] = profit[0];
+            }
+        }
+
+        for (let r = 1; r < rows; r++) {
+            const curRow = new Array(cols).fill(0);
+            for (let c = 1; c < cols; c++) {
+                let skip = dp[c];
+                let include = 0;
+                if (c - weight[r] >= 0) {
+                    include = profit[r] + dp[c - weight[r]];
+                }
+
+                curRow[c] = Math.max(include, skip);
+            }
+            dp = curRow;
+        }
+
+        return dp[cols - 1];
+    }
+}
