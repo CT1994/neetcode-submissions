3bdb83548b550ccc4405d4ad7ce9769e7c1eca14
CT1994 <CT1994@users.noreply.github.com>
2026-07-29 17:01:34 +0100

    Add: ones-and-zeroes - submission-6

---
+++ Data Structures & Algorithms/ones-and-zeroes/submission-6.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    findMaxForm(strs, m, n) {
+        const arr = Array.from({ length: strs.length }, () => [0, 0]);
+        for (let i = 0; i < strs.length; i++) {
+            for (const c of strs[i]) {
+                arr[i][c - "0"]++;
+            }
+        }
+
+        const dp = {};
+        const dfs = (i, m, n) => {
+            if (i === strs.length) return 0;
+            const key = `${i}-${m}-${n}`;
+            if (key in dp) return dp[key];
+            let res = dfs(i + 1, m, n);
+            if (m >= arr[i][0] && n >= arr[i][1]) {
+                res = Math.max(res, 1 + dfs(i + 1, m - arr[i][0], n - arr[i][1]));
+            }
+
+            return (dp[key] = res);
+        };
+
+        return dfs(0, m, n);
+    }
+}
