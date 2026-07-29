3ada70fd2476610423797f713990f1abddfb7df9
CT1994 <CT1994@users.noreply.github.com>
2026-07-29 16:08:38 +0100

    Add: permutations-ii - submission-4

---
+++ Data Structures & Algorithms/permutations-ii/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permuteUnique(nums) {
+        let perms = [[]];
+
+        for (const n of nums) {
+            const nextPerms = [];
+            for (const perm of perms) {
+                for (let i = 0; i < perm.length + 1; i++) {
+                    nextPerms.push(perm.toSpliced(i, 0, n));
+
+                    if (perm[i] === n) break;
+                }
+            }
+            perms = nextPerms;
+        }
+
+        return perms;
+    }
+}
