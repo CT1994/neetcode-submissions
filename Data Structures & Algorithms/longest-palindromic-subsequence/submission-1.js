d5abdf558435d94744d47a65d32b40a0e0b4b6f1
CT1994 <CT1994@users.noreply.github.com>
2026-07-31 12:44:31 +0100

    Add: longest-palindromic-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-subsequence/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    longestPalindromeSubseq(s) {
+        const dp = Array.from({ length: s.length }, () => new Array(s.length).fill(-1));
+        const dfs = (i, j) => {
+            if (i > j) return 0;
+            if (i === j) return 1;
+            if (dp[i][j] !== -1) return dp[i][j];
+            if (s[i] === s[j]) return (dp[i][j] = 2 + dfs(i + 1, j - 1));
+            return (dp[i][j] = Math.max(dfs(i + 1, j), dfs(i, j - 1)));
+        };
+        return dfs(0, s.length - 1);
+    }
+}
