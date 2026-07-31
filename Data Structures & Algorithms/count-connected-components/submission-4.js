41f9f71a1a6a4b986a4366667c6a55f131b9d831
CT1994 <CT1994@users.noreply.github.com>
2026-07-31 11:34:59 +0100

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.js
@@ -0,0 +1,42 @@
+class UnionFind {
+    constructor(n) {
+        this.par = Array.from({ length: n }, (_, i) => i);
+        this.rank = new Array(n).fill(0);
+        this.components = n;
+    }
+
+    find(x) {
+        if (x === this.par[x]) return x;
+        return (this.par[x] = this.find(this.par[x]));
+    }
+
+    union(x, y) {
+        const p1 = this.find(x);
+        const p2 = this.find(y);
+
+        if (p1 === p2) return false;
+
+        if (this.rank[p1] > this.rank[p2]) {
+            this.par[p2] = p1;
+        } else if (this.rank[p1] < this.rank[p2]) {
+            this.par[p1] = p2;
+        } else {
+            this.par[p2] = p1;
+            this.rank[p1]++;
+        }
+        this.components--;
+        return true;
+    }
+}
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        const uf = new UnionFind(n);
+        for (const [n1, n2] of edges) uf.union(n1, n2);
+        return uf.components;
+    }
+}
