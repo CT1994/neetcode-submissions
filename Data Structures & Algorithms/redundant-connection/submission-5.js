4ae32bd0c1a49f1ddf4bb98873472a699656aedc
CT1994 <CT1994@users.noreply.github.com>
2026-07-31 13:03:01 +0100

    Add: redundant-connection - submission-5

---
+++ Data Structures & Algorithms/redundant-connection/submission-5.js
@@ -0,0 +1,40 @@
+class UnionFind {
+    constructor(n) {
+        this.par = Array.from({ length: n }, (_, i) => i);
+        this.rank = new Array(n).fill(0);
+    }
+
+    find(x) {
+        if (x === this.par[x]) return x;
+        return (this.par[x] = this.find(this.par[x]));
+    }
+
+    union(x, y) {
+        const p1 = this.find(x);
+        const p2 = this.find(y);
+
+        if (p1 === p2) return false;
+
+        if (this.rank[p1] > this.rank[p2]) {
+            this.par[p2] = p1;
+        } else if (this.rank[p1] < this.rank[p2]) {
+            this.par[p1] = p2;
+        } else {
+            this.par[p2] = p1;
+            this.rank[p1]++;
+        }
+        return true;
+    }
+}
+class Solution {
+    /**
+     * @param {number[][]} edges
+     * @return {number[]}
+     */
+    findRedundantConnection(edges) {
+        const uf = new UnionFind(edges.length + 1);
+        for (const [n1, n2] of edges) {
+            if (!uf.union(n1, n2)) return [n1, n2];
+        }
+    }
+}
