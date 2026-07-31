5694f3efabd54af429e241942ec71e8451359b88
CT1994 <CT1994@users.noreply.github.com>
2026-07-31 12:52:38 +0100

    Add: quickSort - submission-7

---
+++ Data Structures & Algorithms/quickSort/submission-7.js
@@ -0,0 +1,38 @@
+/** Pair class to store key-value pairs */
+// class Pair {
+//   /**
+//    * @param {number} key The key to be stored in the pair
+//    * @param {string} value The value to be stored in the pair
+//    */
+//   constructor(key, value) {
+//       this.key = key;
+//       this.value = value;
+//   }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[]}
+     */
+    quickSort(pairs) {
+        this.quickSortHelper(pairs, 0, pairs.length - 1);
+        return pairs;
+    }
+
+    quickSortHelper(pairs, l, r) {
+        if (r - l + 1 > 1) {
+            const pivot = pairs[r];
+            let k = l;
+            for (let i = l; i < r; i++) {
+                if (pairs[i].key < pivot.key) {
+                    [pairs[k], pairs[i]] = [pairs[i], pairs[k]];
+                    k++;
+                }
+            }
+
+            [pairs[r], pairs[k]] = [pairs[k], pairs[r]];
+            this.quickSortHelper(pairs, l, k - 1);
+            this.quickSortHelper(pairs, k + 1, r);
+        }
+    }
+}
