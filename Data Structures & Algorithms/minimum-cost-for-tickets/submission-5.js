dbed5d478c1fb29e838b3c1d3bd3598244ef4417
CT1994 <CT1994@users.noreply.github.com>
2026-08-09 18:55:26 +0100

    Add: minimum-cost-for-tickets - submission-5

---
+++ Data Structures & Algorithms/minimum-cost-for-tickets/submission-5.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} days
+     * @param {number[]} costs
+     * @return {number}
+     */
+    mincostTickets(days, costs) {
+        const dp = new Array(days.length + 1).fill(-1);
+        const dfs = (i) => {
+            if (i >= days.length) return 0;
+            if (dp[i] !== -1) return dp[i];
+            const oneDay = costs[0] + dfs(i + 1);
+
+            let j = i;
+            while (j < days.length && days[j] < days[i] + 7) j++;
+            const sevenDays = costs[1] + dfs(j);
+
+            let k = i;
+            while (k < days.length && days[k] < days[i] + 30) k++;
+            const thirtyDays = costs[2] + dfs(k);
+
+            return (dp[i] = Math.min(oneDay, sevenDays, thirtyDays));
+        };
+        return dfs(0);
+    }
+}
