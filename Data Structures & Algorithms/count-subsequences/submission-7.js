32e3e2a6709cc29b6a5bdc7ed11ab194add64639
CT1994 <CT1994@users.noreply.github.com>
2026-08-19 10:58:01 +0100

    Add: count-subsequences - submission-7

---
+++ Data Structures & Algorithms/count-subsequences/submission-7.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {number}
+     */
+    numDistinct(s, t) {
+        const dp = Array.from({ length: s.length + 1 }, () => new Array(t.length + 1).fill(-1));
+        const dfs = (i, j) => {
+            if (j === t.length) return 1;
+            if (i === s.length) return 0;
+            if (dp[i][j] !== -1) return dp[i][j];
+
+            let count = 0;
+            if (s[i] === t[j]) {
+                count += dfs(i + 1, j + 1);
+            }
+
+            return (dp[i][j] = count + dfs(i + 1, j));
+        };
+        return dfs(0, 0);
+    }
+}
