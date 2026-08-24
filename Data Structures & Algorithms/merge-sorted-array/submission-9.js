a3663eb987b905148848303c9fadc61252f02701
CT1994 <CT1994@users.noreply.github.com>
2026-08-24 10:18:17 +0100

    Add: merge-sorted-array - submission-9

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-9.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number} m
+     * @param {number[]} nums2
+     * @param {number} n
+     * @return {void} Do not return anything, modify nums1 in-place instead.
+     */
+    merge(nums1, m, nums2, n) {
+        let k = m + n - 1;
+        m--;
+        n--;
+        while (n >= 0) {
+            if (m >= 0 && nums1[m] > nums2[n]) {
+                nums1[k--] = nums1[m--];
+            } else {
+                nums1[k--] = nums2[n--];
+            }
+        }
+    }
+}
